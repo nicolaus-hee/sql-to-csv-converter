905785f4916aa9c98c0ef453f73bddc691ebb57c
nicolaus-hee <nico.heereman@gmail.com>
2023-01-07 17:54:10 +0100

    initial commit

---
+++ sample.sql
@@ -0,0 +1,37 @@
+-- --------------------------------------------------------
+-- Host:                         sql123.abc.org
+-- Server Version:               10.5.18-MariaDB-0+deb11u1 - Debian 11
+-- Server Betriebssystem:        debian-linux-gnu
+-- HeidiSQL Version:             12.3.0.6589
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Exportiere Daten aus Tabelle test_name.table_name: ~111.280.314 rows
+INSERT INTO `table_name` (`statusID`, `status`, `equipment`, `timestamp`, `user`, `seconds_since_last`) VALUES
+        (769506, 'ACTIVE', 10457770, '2018-01-02 16:50:42', '', 720706),
+        (769507, 'ACTIVE', 10457810, '2018-01-02 16:50:42', '', 720705),
+        (769508, 'ACTIVE', 10457932, '2018-01-02 16:50:43', '', 720706),
+        (769509, 'ACTIVE', 10457933, '2018-01-02 16:50:44', '', 720706),
+        (769510, 'ACTIVE', 10457999, '2018-01-02 16:50:44', '', 720705),
+        (769511, 'ACTIVE', 10458000, '2018-01-02 16:50:45', '', 720705),
+        (769512, 'ACTIVE', 10458218, '2018-01-02 16:50:46', '', 720706),
+        (769513, 'ACTIVE', 10458219, '2018-01-02 16:50:46', '', 720705),
+        (769514, 'ACTIVE', 10458220, '2018-01-02 16:50:47', '', 720705);
+INSERT INTO `table_name` (`statusID`, `status`, `equipment`, `timestamp`, `user`, `seconds_since_last`) VALUES
+        (769506, 'ACTIVE', 10457770, '2018-01-02 16:50:42', '', 720706),
+        (769507, 'ACTIVE', 10457810, '2018-01-02 16:50:42', '', 720705),
+        (769508, 'ACTIVE', 10457932, '2018-01-02 16:50:43', '', 720706),
+        (769509, 'ACTIVE', 10457933, '2018-01-02 16:50:44', '', 720706),
+        (769510, 'ACTIVE', 10457999, '2018-01-02 16:50:44', '', 720705),
+        (769511, 'ACTIVE', 10458000, '2018-01-02 16:50:45', '', 720705),
+        (769512, 'ACTIVE', 10458218, '2018-01-02 16:50:46', '', 720706),
+        (769513, 'ACTIVE', 10458219, '2018-01-02 16:50:46', '', 720705),
+        (769514, 'ACTIVE', 10458220, '2018-01-02 16:50:47', '', 720705);
---
+++ sql_to_csv.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+from datetime import datetime
+
+def is_insert_line(line: str) -> bool:
+    if("INSERT INTO" in line):
+        return True
+    else:
+        return False
+
+def first_insert_line(source_file: str) -> int:
+    i = 0
+    with open(source_file, "r") as infile:
+        for line in infile:
+            i += 1
+            if(is_insert_line(line)):
+                return i
+    return -1
+
+def find_nth_occurrence(haystack: str, needle: str, n: int) -> int:
+    index = 0
+    start_pos = 0
+    for i in range(1, n + 1):
+        index = haystack.find(needle, start_pos)
+        if(i == n):
+            return index
+        start_pos = index + 1
+    return -1
+
+def convert_line(line: str) -> str:
+    replace_terms = {
+        "(": "",
+        "),": "",
+        ");": "",
+        "	": "",
+        "        ": "",
+        ", ": ",",
+        "`": "",
+        ") VALUES": ""
+    }
+
+    for r in replace_terms:
+        line = line.replace(r, replace_terms[r])
+
+    if(is_insert_line(line)):
+        line = line[find_nth_occurrence(line, " ", 3)+1:]
+
+    return line
+
+def convert_file(source_file: str, target_file: str) -> bool:
+    i = 0
+    skip_lines = first_insert_line(source_file) - 1
+
+    with open(source_file, "r") as infile:
+        append_text = ""
+        headers_added = False
+
+        for line in infile:
+            i += 1
+            if(i > skip_lines):
+                if not (is_insert_line(line)):
+                        append_text += convert_line(line)
+                else:
+                    if (headers_added == False):
+                        append_text += convert_line(line)
+                        headers_added = True
+
+            if(i % 10000 == 0):
+                append_line(target_file, append_text)
+                append_text = ""
+
+            # print status every 10k iterations
+            if(i % 10000 == 0):
+                print(str(datetime.now()) + " --> " + str(i))
+
+        # append what is left over > 10k lines
+        append_line(target_file, append_text)
+    return True
+
+def append_line(target_file: str, line: str) -> bool:
+    with open(target_file, "a") as file:
+        file.write(line)
+    return True
+
+def main():   
+    print(str(datetime.now()) + " --> start")
+    convert_file("sample.sql", "sample.csv")
+    print(str(datetime.now()) + " --> end")
+
+if __name__ == "__main__":
+    main()
